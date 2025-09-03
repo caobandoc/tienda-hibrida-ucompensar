ce74f9861944b65d9d9cf144c0dede58285542c6
caobandoc <obandocarlosandres@gmail.com>
2025-09-02 22:02:23 -0500

    add service auth

---
+++ src/app/core/services/auth.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User, UserRegister} from "../models/User";
+import {Token} from "../models/Token";
+
+const { apiUrl } = environment;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+
+  private readonly url: string = `${apiUrl}/auth`;
+
+  constructor(private readonly http: HttpClient) {}
+
+  public register(user: UserRegister): Observable<UserRegister>{
+    return this.http.post<UserRegister>(`${this.url}/register`, user);
+  }
+
+  public login(user: User): Observable<Token> {
+    return this.http.post<Token>(`${this.url}/login`, user);
+  }
+}
