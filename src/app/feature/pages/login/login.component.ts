5c1d9523d7836d7f4b634558ce6c438807058fa9
caobandoc <obandocarlosandres@gmail.com>
2025-09-03 21:23:38 -0500

    base login

--- src/app/feature/pages/login/login.component.ts
+++ src/app/feature/pages/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  imports: [
+    IonicModule,
+    FormsModule
+  ]
 })
 export class LoginComponent {
-
-  constructor() { }
+  email: string = '';
+  password: string = '';
 
 }
